a6a16fe7c11bd67cf4723a7756f8269331cf9f6c
Lucas Lisboa <lucaslisboa1563@gmail.com>
2022-03-22 10:19:08 -0400

    LifeCycleHooks

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Primeiro Componente',
+  data(){
+      return{
+          nome: "Lucas",
+          profissao: "Programador"
+        }
+    }
+ }
+</script>
